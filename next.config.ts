cfe368cad71fba10b085772d1ef9cbd14ae8fb44
islamux <fathi733l@gmail.com>
2025-07-29 11:26:00 +0300

    Configure ESLint to allow warnings during production build

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,11 @@ const withNextIntl = createNextIntlPlugin('./src/i18n/request.ts');
 
 const nextConfig: NextConfig = {
   /* config options here */
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default withNextIntl(nextConfig);
